fc2532484d7a69cf697f550bc79647f4ec99b6f4
Zz <quitjie@gmail.com>
2020-07-30 13:12:07 +0800

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -5,13 +5,11 @@
  * Created Date: 2017-01-22 08:59:15
  * Author: Zz
  * -----
- * Last Modified: 2020-07-29 22:07:46
+ * Last Modified: 2020-07-30 11:06:57
  * Modified By: Zz
  * -----
  * Description:
  */
-
-import Pkg from '../../package.json'
 import './env'
 
 const port = process.env.PORT || 3000
@@ -22,18 +20,20 @@ const mysqlPassword = process.env.MYSQL_PASSWORD || ''
 const mysqlHost = process.env.MYSQL_HOST || '127.0.0.1'
 const mysqlPort = process.env.MYSQL_PORT || 3306
 
+const name = 'z-ali-fc'
+
 const redisCacheTTL = process.env.REDIS_CACHE_TTL || 60
-const redisPrefix = process.env.REDIS_PREFIX || Pkg.name
+const redisPrefix = process.env.REDIS_PREFIX || name
 const redisHost = process.env.REDIS_HOST || '127.0.0.1'
 const redisPort = process.env.REDIS_PORT || '6379'
 const redisDB = process.env.REDIS_DB || '0'
 const redisPassword = process.env.REDIS_PASSWORD || ''
 
 export default {
-  name: Pkg.name,
+  name,
+  version: '1.0.0',
   env: process.env.NODE_ENV,
   port,
-  version: Pkg.version,
   serviceName: 'seneca',
   cache: {
     options: {
